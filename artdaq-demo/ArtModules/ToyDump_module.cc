08eff7c78af0eb2d5fc7018350aaa91393beed2f
Kurt Biery <biery@fnal.gov>
2016-02-29 15:41:05 -0600

    Added printout of ADC values to console.

--- artdaq-demo/ArtModules/ToyDump_module.cc
+++ artdaq-demo/ArtModules/ToyDump_module.cc
@@ -37,6 +37,9 @@ private:
   std::string raw_data_label_;
   std::string frag_type_;
   uint32_t num_adcs_to_show_;
+  bool dump_to_file_;
+  bool dump_to_screen_;
+  uint32_t columns_to_display_on_screen_;
 };
 
 
@@ -44,8 +47,10 @@ demo::ToyDump::ToyDump(fhicl::ParameterSet const & pset)
     : EDAnalyzer(pset),
       raw_data_label_(pset.get<std::string>("raw_data_label")),
       frag_type_(pset.get<std::string>("frag_type")),
-      num_adcs_to_show_(pset.get<uint32_t>("num_adcs_to_show", 0))
-		      
+      num_adcs_to_show_(pset.get<uint32_t>("num_adcs_to_show", 0)),
+      dump_to_file_(pset.get<bool>("dump_to_file", true)),
+      dump_to_screen_(pset.get<bool>("dump_to_screen", false)),
+      columns_to_display_on_screen_(pset.get<uint32_t>("columns_to_display_on_screen", 10))
 {
 }
 
@@ -81,7 +86,7 @@ void demo::ToyDump::analyze(art::Event const & evt)
       std::cout << std::endl;
       std::cout << "Toy fragment " << frag.fragmentID() << " has total ADC counts = " 
 		<< bb.total_adc_values() << std::endl;
-      std::cout << std::endl;
+      //std::cout << std::endl;
 
       if (frag.hasMetadata()) {
       std::cout << std::endl;
@@ -94,7 +99,7 @@ void demo::ToyDump::analyze(art::Event const & evt)
 		  << " -> max ADC value = " 
 		  << bb.adc_range( (int)md->num_adc_bits )
                   << std::endl;
-	std::cout << std::endl;
+	//std::cout << std::endl;
       }
 
       if (num_adcs_to_show_ == 0) {
@@ -112,12 +117,31 @@ void demo::ToyDump::analyze(art::Event const & evt)
 		    << std::endl;
 	}
 
-	std::ofstream output ("out.bin", std::ios::out | std::ios::app | std::ios::binary );
-	for (uint32_t i_adc = 0; i_adc < num_adcs_to_show_; ++i_adc) {
-	  output.write((char*)(bb.dataBegin() + i_adc),sizeof(ToyFragment::adc_t));
-	}
-        output.close();
-	std::cout << std::endl;
+        if (dump_to_file_) {
+          std::ofstream output ("out.bin", std::ios::out | std::ios::app | std::ios::binary );
+          for (uint32_t i_adc = 0; i_adc < num_adcs_to_show_; ++i_adc) {
+            output.write((char*)(bb.dataBegin() + i_adc),sizeof(ToyFragment::adc_t));
+          }
+          output.close();
+        }
+
+        if (dump_to_screen_) {
+          std::cout << std::right;
+          int rows = 1 + (int) ((num_adcs_to_show_ - 1) / columns_to_display_on_screen_);
+          uint32_t adc_counter = 0;
+          for (int idx = 0; idx < rows; ++idx) {
+            std::cout << std::setw(4) << std::setfill('.');
+            std::cout << (idx * columns_to_display_on_screen_) << ": ";
+            for (uint32_t jdx = 0; jdx < columns_to_display_on_screen_; ++jdx) {
+              if (adc_counter >= num_adcs_to_show_) {break;}
+              std::cout << std::setw(6) << std::setfill(' ');
+              std::cout << (bb.dataBegin()[adc_counter]);
+              ++adc_counter;
+            }
+            std::cout << std::endl;
+          }
+        }
+
 	std::cout << std::endl;
       }
     }
