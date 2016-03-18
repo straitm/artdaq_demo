6cc82e949ca43449d7af5c280f3715c4dea04d6a
John Freeman <jcfree@fnal.gov>
2016-03-18 13:59:20 -0500

    JCF: overhauled ToySimulator + added ToyHardwareInterface. Still need to debug WFViewer issue

---
+++ artdaq-demo/Generators/ToyHardwareInterface/ToyHardwareInterface.cc
@@ -0,0 +1,103 @@
+
+#include "artdaq-demo/Generators/ToyHardwareInterface/ToyHardwareInterface.hh"
+#include "artdaq-core-demo/Overlays/FragmentType.hh"
+
+#include "fhiclcpp/ParameterSet.h"
+#include "cetlib/exception.h"
+
+#include <random>
+#include <unistd.h>
+
+// JCF, Mar-17-2016
+
+// ToyHardwareInterface is meant to mimic a vendor-provided hardware
+// API, usable within the the ToySimulator fragment generator. For
+// purposes of realism, it's a C++03-style API, as opposed to, say, one
+// based in C++11 capable of taking advantage of smart pointers, etc.
+
+ToyHardwareInterface::ToyHardwareInterface(fhicl::ParameterSet const & ps) :
+  interface_configured_(false),
+  nADCcounts_(ps.get<size_t>("nADCcounts", 600000)), 
+  fragment_type_(demo::toFragmentType(ps.get<std::string>("fragment_type"))), 
+  throttle_usecs_(ps.get<size_t>("throttle_usecs", 100000)),
+  engine_(ps.get<int64_t>("random_seed", 314159)),
+  uniform_distn_(new std::uniform_int_distribution<data_t>(0, pow(2, NumADCBits() ) - 1 ))
+{
+}
+
+// JCF, Mar-18-2017
+
+// "ConfigureInterface" is meant to mimic the uploading of values to registers, etc. 
+
+void ToyHardwareInterface::ConfigureInterface() {
+  interface_configured_ = true;
+}
+
+
+void ToyHardwareInterface::FillBuffer(char* buffer, size_t* bytes_read) {
+
+  if (interface_configured_) {
+
+    usleep( throttle_usecs_ );
+
+    // ASSUMES NO HEADER YET
+    *bytes_read = nADCcounts_ * sizeof(data_t);
+      
+    // Make the fake data
+
+    // Header?
+
+    // Generate nADCcounts ADC values ranging from 0 to max with an
+    // equal probability over the full range (a specific and perhaps
+    // not-too-physical example of how one could generate simulated
+    // data)
+
+    std::generate_n(reinterpret_cast<data_t*>(buffer), nADCcounts_,
+		    [&]() {
+		      return static_cast<data_t>
+			((*uniform_distn_)( engine_ ));
+		    }
+		    );
+
+  } else {
+    // what to do here? std::exit(1) ??
+  }
+}
+
+void ToyHardwareInterface::AllocateReadoutBuffer(char** buffer) {
+  
+  *buffer = reinterpret_cast<char*>( new uint8_t[ nADCcounts_*sizeof(data_t) ] );
+}
+
+void ToyHardwareInterface::FreeReadoutBuffer(char* buffer) {
+  delete [] buffer;
+}
+
+uint8_t ToyHardwareInterface::BoardType() const {
+  return fragment_type_;
+}
+
+uint8_t ToyHardwareInterface::NumADCBits() const {
+
+  switch (fragment_type_) {
+  case demo::FragmentType::TOY1:
+    return 12;
+    break;
+  case demo::FragmentType::TOY2:
+    return 14;
+    break;
+  default:
+    throw cet::exception("ToyHardwareInterface")
+      << "Unknown board type "
+      << fragment_type_
+      << " ("
+      << demo::fragmentTypeToString(fragment_type_)
+      << ").\n";
+  };
+
+}
+
+uint16_t ToyHardwareInterface::SerialNumber() const {
+  return 999;
+}
+
---
+++ artdaq-demo/Generators/ToyHardwareInterface/ToyHardwareInterface.hh
@@ -0,0 +1,60 @@
+#ifndef artdaq_demo_Generators_ToyHardwareInterface_ToyHardwareInterface_hh
+#define artdaq_demo_Generators_ToyHardwareInterface_ToyHardwareInterface_hh
+
+// JCF, Mar-17-2016
+
+// ToyHardwareInterface is meant to mimic a vendor-provided hardware
+// API, usable within the the ToySimulator fragment generator. For
+// purposes of realism, it's a C++03-style API, as opposed to, say,
+// one based in C++11 capable of taking advantage of smart pointers,
+// etc. An important point to make is that it has ownership of the
+// buffer into which it dumps its data - so rather than use
+// new/delete, use its functions
+// AllocateReadoutBuffer/FreeReadoutBuffer
+
+// The data it returns are ADC counts distributed according to the
+// uniform distribution
+
+#include "artdaq-core-demo/Overlays/FragmentType.hh"
+
+#include "fhiclcpp/fwd.h"
+
+#include <random>
+
+
+class ToyHardwareInterface {
+
+public:
+
+  typedef uint16_t data_t;
+
+  ToyHardwareInterface(fhicl::ParameterSet const & ps);
+
+  void ConfigureInterface();
+  void FillBuffer(char* buffer, size_t* bytes_read);
+
+  void AllocateReadoutBuffer(char** buffer);
+  void FreeReadoutBuffer(char* buffer);
+
+  uint16_t SerialNumber() const;
+  uint8_t NumADCBits() const;
+  uint8_t BoardType() const;
+
+private:
+
+  bool interface_configured_;
+  std::size_t nADCcounts_;
+  demo::FragmentType fragment_type_;
+  std::size_t throttle_usecs_;
+
+// Members needed to generate the simulated data
+
+  std::mt19937 engine_;
+  std::unique_ptr<std::uniform_int_distribution<data_t>> uniform_distn_;
+
+
+};
+
+
+
+#endif
