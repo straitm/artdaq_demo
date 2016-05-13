61f1efff037c9b4fd4df8b3098ac70d35be649aa
John Freeman <jcfree@fnal.gov>
2016-05-13 17:02:13 -0500

    JCF: modify the examples/metrics/ versions of quick-start.sh and start2x2x2System.sh so that things run out-of-the-box

--- examples/metrics/start2x2x2System.sh
+++ examples/metrics/start2x2x2System.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo `which setupDemoEnvironment.sh`
 source `which setupDemoEnvironment.sh`
 
 # create the configuration file for PMT
@@ -19,6 +20,22 @@ mkdir -p -m 0777 ${logroot}/masterControl
 mkdir -p -m 0777 ${logroot}/boardreader
 mkdir -p -m 0777 ${logroot}/eventbuilder
 mkdir -p -m 0777 ${logroot}/aggregator
+
+if [[ -n $PRODUCTS ]]; then
+    . $PRODUCTS/setup
+else
+    echo "Unable to find $PRODUCTS/setup, exiting..." >&2
+    exit 1
+fi
+
+cmd="setup artdaq_mfextensions v1_0_6 -q e9:prof:s21"
+
+$cmd
+
+if [[ "$?" != "0" ]]; then
+    echo "Problem executing \"$cmd\", exiting..." >&2
+    exit 2
+fi
 
 # If present, start the msgviewer dialog
 msgviewer -c $ARTDAQ_MFEXTENSIONS_FQ_DIR/bin/msgviewer.fcl 2>&1 >/dev/null &
