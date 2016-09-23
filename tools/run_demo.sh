ac37023cd3c589da32238666082e224f3d168fcc
John Freeman <jcfree@fnal.gov>
2016-09-23 14:45:34 -0500

    JCF: in run_demo.sh, avoid having the WFViewer and ToyDump modules on terminals which overlap

--- tools/run_demo.sh
+++ tools/run_demo.sh
@@ -4,7 +4,6 @@ toolsdir=$2
 
     $toolsdir/xt_cmd.sh $basedir --geom '132x33 -sl 2500' \
         -c '. ./setupARTDAQDEMO' \
-        -c 'cp -p '$toolsdir'/fcl/TransferInputShmem.fcl '$toolsdir'/fcl/TransferInputShmem2.fcl' \
         -c start2x2x2System.sh
     sleep 2
 
@@ -24,14 +23,25 @@ toolsdir=$2
 
     sleep 14;
 
-    $toolsdir/xt_cmd.sh $basedir --geom '132x33 -sl 2500' \
+    xrdbproc=$( which xrdb )
+
+    xloc=
+    if [[ -e $xrdbproc ]]; then
+	xloc=$( xrdb -symbols | grep DWIDTH | awk 'BEGIN {FS="="} {pixels = $NF; print pixels/2}' )
+    else
+	xloc=800
+    fi
+
+    $toolsdir/xt_cmd.sh $basedir --geom '100x33+0+0 -sl 2500' \
         -c '. ./setupARTDAQDEMO' \
         -c 'art -c '$toolsdir'/fcl/TransferInputShmem.fcl'
 
     sleep 4;
 
-    $toolsdir/xt_cmd.sh $basedir --geom '132x33 -sl 2500' \
+    $toolsdir/xt_cmd.sh $basedir --geom '100x33+'$xloc'+0 -sl 2500' \
         -c '. ./setupARTDAQDEMO' \
+	-c 'rm -f '$toolsdir'/fcl/TransferInputShmem2.fcl' \
+        -c 'cp -p '$toolsdir'/fcl/TransferInputShmem.fcl '$toolsdir'/fcl/TransferInputShmem2.fcl' \
 	-c 'sed -r -i "s/.*modulus.*[0-9]+.*/modulus: 10/" '$toolsdir'/fcl/TransferInputShmem2.fcl' \
 	-c 'sed -r -i "/end_paths:/s/a1/a3/" '$toolsdir'/fcl/TransferInputShmem2.fcl' \
 	-c 'sed -r -i "/shm_key:/s/.*/shm_key: 0x40471453/" '$toolsdir'/fcl/TransferInputShmem2.fcl' \
