f361523d26b741304966734fe95dcfbf6292cb98
Eric Flumerfelt <eflumerf@fnal.gov>
2016-12-30 11:19:10 -0600

    Add generate command to manage2x2System.sh. Make the generated BoardReader fcl a bit more readable.

--- tools/generateFragmentReceiver.rb
+++ tools/generateFragmentReceiver.rb
@@ -22,16 +22,21 @@ def generateFragmentReceiver(startingFragmentId, boardId,
   end
 
   fgConfig = String.new( 
-    read_fcl("CommandableFragmentGenerator.fcl") +\
-    "\
+    "# CommandableFragmentGenerator Configuration: " +
+    read_fcl("CommandableFragmentGenerator.fcl") +
+    "
+
+    # Generated Parameters: 
     generator: %{generator}
     fragment_type: %{fragment_type}
     fragment_id: %{starting_fragment_id}
     board_id: %{board_id}
     starting_fragment_id: %{starting_fragment_id}
     random_seed: %{random_seed}
-    sleep_on_stop_us: 500000 " \
-    + read_fcl(configDoc) )
+    sleep_on_stop_us: 500000 
+
+    # Generator-Specific Configuration:
+    " + read_fcl(configDoc) )
   
   fgConfig.gsub!(/\%\{generator\}/, String(generator))
   fgConfig.gsub!(/\%\{fragment_type\}/, String(fragmentType)) 
