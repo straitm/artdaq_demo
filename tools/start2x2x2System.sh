07668acd291a3e0d15f3b57e8f85848c93c71a86
John Freeman <jcfree@mu2edaq01.fnal.gov>
2017-01-04 16:27:50 -0600

    JCF: changes to start2x2x2System.sh and start2x2x4System.sh to fix sending of messages to msgviewer

--- tools/start2x2x2System.sh
+++ tools/start2x2x2System.sh
@@ -24,10 +24,11 @@ if [[ "x${ARTDAQ_MFEXTENSIONS_DIR-}" != "x" ]] && [[ "x${DISPLAY-}" != "x" ]]; t
     configPath=$ARTDAQ_MFEXTENSIONS_DIR/config/msgviewer.fcl
     if [ -n "${ARTDAQ_MFEXTENSIONS_FQ_DIR}" ]; then configPath=${ARTDAQ_MFEXTENSIONS_FQ_DIR}/bin/msgviewer.fcl; fi
     msgviewer -c $configPath 2>&1 >${logroot}/msgviewer.log &
-    echo "udp: { type: \"UDP\" threshold: \"DEBUG\" host: \"127.0.0.1\" port: 30000 }" >${logroot}/MessageFacility.fcl
+    echo "udp: { type: \"UDP\" threshold: \"INFO\" host: \"${HOSTNAME}\" port: 30000 }" >${logroot}/MessageFacility.fcl
     export ARTDAQ_LOG_FHICL=${logroot}/MessageFacility.fcl
 fi
 
 # start PMT
 pmt.rb -p ${ARTDAQDEMO_PMT_PORT} -d $tempFile --logpath ${logroot} --display ${DISPLAY}
 rm $tempFile
+
