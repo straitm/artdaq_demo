ebdfa8111619a2d66d5bbb8d6ffb0457410b72ca
Eric Flumerfelt <eflumerf@fnal.gov>
2017-07-14 14:07:11 -0500

    Add the Source_generator traits to DemoInput so that it actually works.

--- artdaq-demo/ArtModules/DemoInput_source.cc
+++ artdaq-demo/ArtModules/DemoInput_source.cc
@@ -2,6 +2,7 @@
 #include "artdaq/ArtModules/detail/SharedMemoryReader.hh"
 #include "artdaq-core-demo/Overlays/FragmentType.hh"
 #include "art/Framework/Core/InputSourceMacros.h"
+#include "art/Framework/IO/Sources/SourceTraits.h"
 
 #include <string>
 using std::string;
@@ -14,7 +15,20 @@ namespace demo
 	/**
 	 * \brief DemoInput is an art::Source using the detail::RawEventQueueReader class
 	 */
-	typedef art::Source<artdaq::detail::SharedMemoryReader<demo::makeFragmentTypeMap>> DemoInput;
+	typedef art::Source< artdaq::detail::SharedMemoryReader<demo::makeFragmentTypeMap> > DemoInput;
+}
+
+namespace art
+{
+	/**
+	* \brief  Specialize an art source trait to tell art that we don't care about
+	* source.fileNames and don't want the files services to be used.
+	*/
+	template <>
+	struct Source_generator<artdaq::detail::SharedMemoryReader<demo::makeFragmentTypeMap>>
+	{
+		static constexpr bool value = true; ///< Used to suppress use of file services on art Source
+	};
 }
 
 DEFINE_ART_INPUT_SOURCE(demo::DemoInput)
