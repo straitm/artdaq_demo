321028b947f3a50f69ce8032e41665db792a8850
John Freeman <jcfree@mu2edaq01.fnal.gov>
2018-01-03 17:36:57 -0600

    JCF: hacked code Ron and I made to troubleshoot our current inability to launch artdaq processes

--- tools/start2x2x2System.sh
+++ tools/start2x2x2System.sh
@@ -22,17 +22,17 @@ mkdir -p -m 0777 ${logroot}/dispatcher
 mkdir -p -m 0777 ${logroot}/datalogger
 mkdir -p -m 0777 ${logroot}/artdaqart
 
-if [[ "x${ARTDAQ_MFEXTENSIONS_DIR-}" != "x" ]] && [[ "x${DISPLAY-}" != "x" ]]; then
-    configPath=$ARTDAQ_MFEXTENSIONS_DIR/config/msgviewer.fcl
-    if [ -n "${ARTDAQ_MFEXTENSIONS_FQ_DIR}" ]; then configPath=${ARTDAQ_MFEXTENSIONS_FQ_DIR}/bin/msgviewer.fcl; fi
-    msgviewer -c $configPath 2>&1 >${logroot}/msgviewer.log &
-    echo "udp: { type: \"UDP\" threshold: \"DEBUG\" host: \"${HOSTNAME}\" port: 30000 }" >${logroot}/MessageFacility.fcl
-    export ARTDAQ_LOG_FHICL=${logroot}/MessageFacility.fcl
-    echo "Sleeping for 5 seconds to allow MessageViewer time to start"
-    sleep 5
-fi
+# if [[ "x${ARTDAQ_MFEXTENSIONS_DIR-}" != "x" ]] && [[ "x${DISPLAY-}" != "x" ]]; then
+#     configPath=$ARTDAQ_MFEXTENSIONS_DIR/config/msgviewer.fcl
+#     if [ -n "${ARTDAQ_MFEXTENSIONS_FQ_DIR}" ]; then configPath=${ARTDAQ_MFEXTENSIONS_FQ_DIR}/bin/msgviewer.fcl; fi
+#     msgviewer -c $configPath 2>&1 >${logroot}/msgviewer.log &
+#     echo "udp: { type: \"UDP\" threshold: \"DEBUG\" host: \"${HOSTNAME}\" port: 30000 }" >${logroot}/MessageFacility.fcl
+#     export ARTDAQ_LOG_FHICL=${logroot}/MessageFacility.fcl
+#     echo "Sleeping for 5 seconds to allow MessageViewer time to start"
+#     sleep 5
+# fi
 
 # start PMT
-pmt.rb -p ${ARTDAQDEMO_PMT_PORT} -d $tempFile --logpath ${logroot} --display ${DISPLAY}
+pmt.rb -p ${ARTDAQDEMO_PMT_PORT} -d /tmp/pmtConfig.artdaq_v3_test --logpath ${logroot} --display ${DISPLAY}
 rm $tempFile
 
