c89a671e06c7ae896f0466ae6cdbcb0aad125b0a
Eric Flumerfelt <eflumerf@fnal.gov>
2018-01-26 19:27:29 -0600

    Use modern PMT config file format.

--- examples/asciiSimulator/startAsciiSimulatorExample.sh
+++ examples/asciiSimulator/startAsciiSimulatorExample.sh
@@ -11,11 +11,11 @@ export FHICL_FILE_PATH=$ARTDAQDEMO_REPO/examples/asciiSimulator:$FHICL_FILE_PATH
 # create the configuration file for PMT
 tempFile="/tmp/pmtConfig.$$"
 
-echo "BoardReaderMain `hostname` ${ARTDAQDEMO_BR_PORT[0]}" >> $tempFile
-echo "EventBuilderMain `hostname` ${ARTDAQDEMO_EB_PORT[0]}" >> $tempFile
-echo "EventBuilderMain `hostname` ${ARTDAQDEMO_EB_PORT[1]}" >> $tempFile
-echo "DataLoggerMain `hostname` ${ARTDAQDEMO_AG_PORT[0]}" >> $tempFile
-echo "DispatcherMain `hostname` ${ARTDAQDEMO_AG_PORT[1]}" >> $tempFile
+echo "BoardReaderMain!`hostname`!id:${ARTDAQDEMO_BR_PORT[0]} commanderPluginType:xmlrpc" >> $tempFile
+echo "EventBuilderMain!`hostname`!id:${ARTDAQDEMO_EB_PORT[0]} commanderPluginType:xmlrpc" >> $tempFile
+echo "EventBuilderMain!`hostname`!id:${ARTDAQDEMO_EB_PORT[1]} commanderPluginType:xmlrpc" >> $tempFile
+echo "DataLoggerMain!`hostname`!id:${ARTDAQDEMO_AG_PORT[0]} commanderPluginType:xmlrpc" >> $tempFile
+echo "DispatcherMain!`hostname`!id:${ARTDAQDEMO_AG_PORT[1]} commanderPluginType:xmlrpc" >> $tempFile
 
 # create the logfile directories, if needed
 logroot="${ARTDAQDEMO_LOG_DIR:-/tmp}"
@@ -25,15 +25,15 @@ mkdir -p -m 0777 ${logroot}/boardreader
 mkdir -p -m 0777 ${logroot}/eventbuilder
 mkdir -p -m 0777 ${logroot}/aggregator
 
-if [[ "x${ARTDAQ_MFEXTENSIONS_DIR-}" != "x" ]] && [[ "x${DISPLAY-}" != "x" ]]; then
-    configPath=$ARTDAQ_MFEXTENSIONS_DIR/config/msgviewer.fcl
-    if [ -n "${ARTDAQ_MFEXTENSIONS_FQ_DIR}" ]; then configPath=${ARTDAQ_MFEXTENSIONS_FQ_DIR}/bin/msgviewer.fcl; fi
-    msgviewer -c $configPath 2>&1 >${logroot}/msgviewer.log &
-    echo "udp: { type: \"UDP\" threshold: \"DEBUG\" host: \"127.0.0.1\" port: 30000 }" >${logroot}/MessageFacility.fcl
-    export ARTDAQ_LOG_FHICL=${logroot}/MessageFacility.fcl
-    echo "Sleeping for 5 seconds to allow MessageViewer time to start"
-    sleep 5
-fi
+#if [[ "x${ARTDAQ_MFEXTENSIONS_DIR-}" != "x" ]] && [[ "x${DISPLAY-}" != "x" ]]; then
+#    configPath=$ARTDAQ_MFEXTENSIONS_DIR/config/msgviewer.fcl
+#    if [ -n "${ARTDAQ_MFEXTENSIONS_FQ_DIR}" ]; then configPath=${ARTDAQ_MFEXTENSIONS_FQ_DIR}/bin/msgviewer.fcl; fi
+#    msgviewer -c $configPath 2>&1 >${logroot}/msgviewer.log &
+#    echo "udp: { type: \"UDP\" threshold: \"DEBUG\" host: \"127.0.0.1\" port: 30000 }" >${logroot}/MessageFacility.fcl
+#    export ARTDAQ_LOG_FHICL=${logroot}/MessageFacility.fcl
+#    echo "Sleeping for 5 seconds to allow MessageViewer time to start"
+#    sleep 5
+#fi
 
 # start PMT
 pmt.rb -p ${ARTDAQDEMO_PMT_PORT} -d $tempFile --logpath ${logroot} --display ${DISPLAY}
--- examples/requestBasedDataFlow/startRequestBasedDataFlowExample.sh
+++ examples/requestBasedDataFlow/startRequestBasedDataFlowExample.sh
@@ -11,13 +11,13 @@ export FHICL_FILE_PATH=$ARTDAQDEMO_REPO/examples/requestBasedDataFlow:$FHICL_FIL
 # create the configuration file for PMT
 tempFile="/tmp/pmtConfig.$$"
 
-echo "BoardReaderMain `hostname` ${ARTDAQDEMO_BR_PORT[0]}" >> $tempFile
-echo "BoardReaderMain `hostname` ${ARTDAQDEMO_BR_PORT[1]}" >> $tempFile
-echo "BoardReaderMain `hostname` ${ARTDAQDEMO_BR_PORT[2]}" >> $tempFile
-echo "EventBuilderMain `hostname` ${ARTDAQDEMO_EB_PORT[0]}" >> $tempFile
-echo "EventBuilderMain `hostname` ${ARTDAQDEMO_EB_PORT[1]}" >> $tempFile
-echo "AggregatorMain `hostname` ${ARTDAQDEMO_AG_PORT[0]}" >> $tempFile
-echo "AggregatorMain `hostname` ${ARTDAQDEMO_AG_PORT[1]}" >> $tempFile
+echo "BoardReaderMain!`hostname`!id:${ARTDAQDEMO_BR_PORT[0]} commanderPluginType:xmlrpc" >> $tempFile
+echo "BoardReaderMain!`hostname`!id:${ARTDAQDEMO_BR_PORT[1]} commanderPluginType:xmlrpc" >> $tempFile
+echo "BoardReaderMain!`hostname`!id:${ARTDAQDEMO_BR_PORT[2]} commanderPluginType:xmlrpc" >> $tempFile
+echo "EventBuilderMain!`hostname`!id:${ARTDAQDEMO_EB_PORT[0]} commanderPluginType:xmlrpc" >> $tempFile
+echo "EventBuilderMain!`hostname`!id:${ARTDAQDEMO_EB_PORT[1]} commanderPluginType:xmlrpc" >> $tempFile
+echo "DataLoggerMain!`hostname`!id:${ARTDAQDEMO_AG_PORT[0]} commanderPluginType:xmlrpc" >> $tempFile
+echo "DispatcherMain!`hostname`!id:${ARTDAQDEMO_AG_PORT[1]} commanderPluginType:xmlrpc" >> $tempFile
 
 # create the logfile directories, if needed
 logroot="${ARTDAQDEMO_LOG_DIR:-/tmp}"
@@ -27,15 +27,15 @@ mkdir -p -m 0777 ${logroot}/boardreader
 mkdir -p -m 0777 ${logroot}/eventbuilder
 mkdir -p -m 0777 ${logroot}/aggregator
 
-if [[ "x${ARTDAQ_MFEXTENSIONS_DIR-}" != "x" ]] && [[ "x${DISPLAY-}" != "x" ]]; then
-    configPath=$ARTDAQ_MFEXTENSIONS_DIR/config/msgviewer.fcl
-    if [ -n "${ARTDAQ_MFEXTENSIONS_FQ_DIR}" ]; then configPath=${ARTDAQ_MFEXTENSIONS_FQ_DIR}/bin/msgviewer.fcl; fi
-    msgviewer -c $configPath 2>&1 >${logroot}/msgviewer.log &
-    echo "udp: { type: \"UDP\" threshold: \"DEBUG\" host: \"127.0.0.1\" port: 30000 }" >${logroot}/MessageFacility.fcl
-    export ARTDAQ_LOG_FHICL=${logroot}/MessageFacility.fcl
-    echo "Sleeping for 5 seconds to allow MessageViewer time to start"
-    sleep 5
-fi
+#if [[ "x${ARTDAQ_MFEXTENSIONS_DIR-}" != "x" ]] && [[ "x${DISPLAY-}" != "x" ]]; then
+#    configPath=$ARTDAQ_MFEXTENSIONS_DIR/config/msgviewer.fcl
+#    if [ -n "${ARTDAQ_MFEXTENSIONS_FQ_DIR}" ]; then configPath=${ARTDAQ_MFEXTENSIONS_FQ_DIR}/bin/msgviewer.fcl; fi
+#    msgviewer -c $configPath 2>&1 >${logroot}/msgviewer.log &
+#    echo "udp: { type: \"UDP\" threshold: \"DEBUG\" host: \"127.0.0.1\" port: 30000 }" >${logroot}/MessageFacility.fcl
+#    export ARTDAQ_LOG_FHICL=${logroot}/MessageFacility.fcl
+#    echo "Sleeping for 5 seconds to allow MessageViewer time to start"
+#    sleep 5
+#fi
 
 # start PMT
 pmt.rb -p ${ARTDAQDEMO_PMT_PORT} -d $tempFile --logpath ${logroot} --display ${DISPLAY}
