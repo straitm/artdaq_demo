66a7db5bf185161829b6835eca622e9ad567397b
Matthew Strait <strait@physics.umn.edu>
2018-05-04 16:12:10 -0500

    Pare down CRTDump and print event.
    Remove the unused parameters from the toy example.
    Use new functionaility from CRTFragment to check
    and print the events.

--- artdaq-demo/ArtModules/CRTDump_module.cc
+++ artdaq-demo/ArtModules/CRTDump_module.cc
@@ -18,10 +18,7 @@
 #include "artdaq-core/Data/Fragment.hh"
 
 #include <algorithm>
-#include <cassert>
-#include <cmath>
 #include <fstream>
-#include <iomanip>
 #include <vector>
 #include <iostream>
 
@@ -32,77 +29,56 @@ namespace demo
 }
 
 /**
- * \brief An art::EDAnalyzer module designed to display the data from demo::CRTFragment objects
+ * \brief art::EDAnalyzer to display the data from CRTFragment objects
  */
 class demo::CRTDump : public art::EDAnalyzer
 {
 public:
   /**
-   * \brief CRTDump Constructor
+   * \brief CRTDump constructor
    * \param pset ParamterSet used to configure CRTDump
    *
-   * \verbatim
-   * CRTDump accepts the following Parameters:
-   * "raw_data_label" (Default: "daq"): The label used to identify artdaq data
-   * "num_adcs_to_show" (Default: 0): How many ADCs to print from each CRTFragment
-   * "dump_to_file" (Default: true): Whether to write data to a binary file "out.bin"
-   * "dump_to_screen" (Default: false): Whether to write data to stdout
-   * "columns_to_display_on_screen" (Default: 10): How many ADC values to print in each row when writing to stdout
-   * \endverbatim
+   * CRTDump accepts the following parameters:
+   * "raw_data_label" (Default: "daq"): The input module label
+   * "raw_data_name"  (Default: "CRT"): The input product instance name
    */
   explicit CRTDump(fhicl::ParameterSet const& pset);
 
-  /**
-   * \brief CRTDump Destructor
-   */
   virtual ~CRTDump();
 
-  /**
-  * \brief Analyze an event. Called by art for each event in run (based on command line options)
-  * \param evt The art::Event object to dump CRTFragments from
-  */
   virtual void analyze(art::Event const& evt);
 
 private:
   std::string raw_data_label_;
-  int num_adcs_to_write_;
-  int num_adcs_to_print_;
-  bool dump_to_file_;
-  bool dump_to_screen_;
-  uint32_t columns_to_display_on_screen_;
-  std::string output_file_name_;
+  std::string raw_data_name_;
 };
-
 
 demo::CRTDump::CRTDump(fhicl::ParameterSet const& pset)
   : EDAnalyzer(pset)
   , raw_data_label_(pset.get<std::string>("raw_data_label", "daq"))
-  , num_adcs_to_write_(pset.get<int>("num_adcs_to_write", 0))
-  , num_adcs_to_print_(pset.get<int>("num_adcs_to_print", 10))
-  , columns_to_display_on_screen_(pset.get<uint32_t>("columns_to_display_on_screen", 10))
-  , output_file_name_(pset.get<std::string>("output_file_name", "out.bin"))
-{}
+  , raw_data_name_ (pset.get<std::string>("raw_data_name",  "CRT"))
+{
+}
 
 demo::CRTDump::~CRTDump() {}
 
 void demo::CRTDump::analyze(art::Event const& evt)
 {
-  printf("Analyzing CRT Fragments\n");
+  art::Handle<artdaq::Fragments> fragments;
 
-  std::vector<std::string> fragment_type_labels{ "CRT", "ContainerCRT" };
+  evt.getByLabel(raw_data_label_, raw_data_name_, fragments);
+  if(!fragments.isValid()){
+    fprintf(stderr, "No product with label \"%s\" and name \"%s\".\n",
+            raw_data_label_.c_str(), raw_data_name_.c_str());
+    return;
+  }
 
-  for (auto label : fragment_type_labels)
-  {
-    art::Handle<artdaq::Fragments> fragments;
+  for(unsigned int i = 0; i < fragments->size(); i++){
+    CRTFragment mod((*fragments)[i]); // module packet
 
-    evt.getByLabel(raw_data_label_, label, fragments);
-    if(!fragments.isValid()) continue;
-
-    for(unsigned int i = 0; i < fragments->size(); i++){
-      CRTFragment mod((*fragments)[i]); // module packet
-
-      printf("Number of hits: %lu\n", mod.num_hits());
-    }
+    if(!mod.good_event()) continue;
+    mod.print_header();
+    mod.print_hits();
   }
 }
 
